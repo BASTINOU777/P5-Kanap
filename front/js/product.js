0cdb49c29c16b53567fb5995420bc81bca578760
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-07-11 16:55:14 +0200

    appel API page d'accueil & affichage element dynamique

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,4 @@
-getArticle();
 
-function getArticle() {
-    fetch("http://localhost:3000/api/products/" + idProduct)
-    .then((res) => {
-        return res.json();
-    })
-}
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,47 @@
-console.log("le javascript marche")
 
-// récuperer les éléments du DOM
-let containerElt = document.querySelector("#items")
-console.log(containerElt);
+/*--------------------------------------------------------------------
+APPEL DE l'API POUR IMPORTER LES PRODUITS A LA PAGE D'ACCEUIl
+--------------------------------------------------------------*/
 
-//récuperer les produits de l'API 
-//fetch ('http://localhost:3000/api/products').then
+fetch ('http://localhost:3000/api/products')
+//récuperation de la réponse pour la convertir en .JSON 
+.then (function(res){
+    if (res.ok){
+        return res.json();
+    }
+})
+.then(function(value) {
+    newArticles(value);
+ })
 
+// Si Api est down alerte "products is not defined"
+.catch(function(error){
+    alert(error);
+    console.log(error);
+})
 
+/*--------------------------------------------------------
+AFFICHAGE DYNAMIQUE DES ELEMENTS DANS LA PAGE ACCEUIL
+--------------------------------------------------*/
+// récupération des éléments du DOM avec la variable container
+let container = document.querySelector('#items')
+console.log (container)
 
-
-
-
-
+//--------récupération des éléments avec une boucle for of------------//
+function newArticles(item) {
+// Constante pour récupere l'iD "items" (enfant du main) dans le DOM//
+    const queryItems = document.getElementById("items");
+    for (let i = 0; i < item.length; i++) {
+      const productItem =
+// récupération du code HTML contenant les articles
+        `<a href="./product.html?id=${item[i]._id}">
+        <article>
+            <img src="${item[i].imageUrl}" alt="${item[i].altTxt}">
+            <h3 class="productName">${item[i].name}</h3>
+            <p class="productDescription">${item[i].description}</p>  
+        </article>
+    </a>`;
+      queryItems.insertAdjacentHTML("beforeend", productItem);
+    }
+  }
+        
