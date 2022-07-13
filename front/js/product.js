f6894c9f01ce730f2faf061f09f6297a99758550
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-07-13 11:22:12 +0200

    requet API page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,18 @@
 /*------------------------------------------------------------
 lien entre produit de la page d’accueil et de la page Produit
 ------------------------------------------------------*/
+
 // utilisation de la méthode window.location.herf pour récuperer l'url de product
 let params = new URL(window.location.href).searchParams;
-
 let newID = params.get("id");
 console.log(newID);
-//récupération des produits dans l'API grâce à la méthode fetch
 
-fetch("http://localhost:3000/api/products/" + newID);
+//récupération des produits dans l'API grâce à la méthode fetch
+fetch("http://localhost:3000/api/products/" + newID)
+  //Si Api fonction resp.ok en .json
+  .then((response) => response.json())
+
+  // Si Api est down alerte "err"
+  .catch((err) => {
+    console.log(err);
+  });
