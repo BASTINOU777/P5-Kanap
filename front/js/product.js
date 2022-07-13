9179f080f48b6887c4814e8f633d7e45d4bb8d9f
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-07-13 13:51:05 +0200

    affichage produits dans la page product

--- front/js/product.js
+++ front/js/product.js
@@ -16,3 +16,37 @@ fetch("http://localhost:3000/api/products/" + newID)
   .catch((err) => {
     console.log(err);
   });
+/*-------------------------------------------------------------
+        AFFICHAGE PRODUIT  
+------------------------------------------------------*/
+
+// fonction des éléments d'un produit
+
+function product(element) {
+  // déclaration des variables des éléments
+  // Image //
+  let imageProduct = docuement.querySelector(".item_img");
+  // Titre //
+  let tittleProduct = document.querySelector("#tittle");
+  // Prix //
+  let priceProduct = document.querySelector("#price");
+  // Description //
+  let descriptionProduct = document.querySelector("#description");
+  // Couleurs //
+  let selectColors = document.querySelector("#colors");
+
+  //Boucle for of pour choisir un élément
+  for (let selectProduct of product) {
+    // si on récupere l'id d'un produit du tableau alors on récupére son indice pour accéder aux donnés stockés
+    if (idProduct === selectProduct._id) {
+      // id Image //
+      imageProduct.innerHTML = `<img src=${selectProduct.imageUrl} alt="${selectProduct.altTxt}">`;
+      // id Titre //
+      tittleProduct.textContent = `${selectProduct.title}`;
+      // id Prix //
+      priceProduct.textContent = `${selectProduct.price}`;
+      // id Description //
+      descriptionProduct = `${selectProduct.description}`;
+    }
+  }
+}
