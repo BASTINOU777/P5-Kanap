c09ee051922c5f903544b0ccd7d69010745eb075
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-07-19 11:33:56 +0200

    derniere version page product changement de pratique

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,36 @@
 /*-------------------------------------------------------------
-        RECUPERATION DU LOCAL STORAGE
+    RECUPERATION DES PRODUITS DU LOCAL STORAGE ET  DE l'API
+---------------------------------------------------------------*/
+//déclaration de ma variable contenant le tableau des articles dans le panier
+let addProducts;
+//console.log(addProducts);
+const FetchProducts = async () => {
+  //attente de la promesse en allant chercher l'API avec fetch
+  await fetch("http://localhost:3000/api/products/")
+    //récuperation de la réponse pour la convertir en .JSON
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      alert(error);
+      // Erreur si pas d'api
+      console.log(error);
+    });
+};
+
+// on récup les infos dans le LS en format json
+let localStorageCart = JSON.parse(localStorage.getItem("cart"));
+//déclaration de ma variable qui récupère le numéro de commande (l'order id) du serveur
+let orderId = "";
+
+/*-------------------------------------------------------------
+        AFFICHAGES DES PRODUITS 
 ------------------------------------------------------*/
-// on récup les infos dans le LS
-//let registerProductOnLocalStorage = JSON.parse(localStorage.getItem("product"));
+//déclaration de ma fonction affichage des produits
+
+function displayCart() {
+  for (let productCart of cart) console.log(productCart);
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,15 @@
 /*------------------------------------------------------------
 lien entre produit de la page d’accueil et de la page Produit
 ------------------------------------------------------*/
-// utilisation de la méthode URLSearchParams pour récuperer l'iD du produit via l'URl
-const urlProduct = new URLSearchParams(document.location.search);
-
-console.log("Affichage de l'url produit :");
-console.log(document.location.search);
-
-const idProduct = urlProduct.get("id");
-
-console.log("Identification de l'id du produit :");
-console.log(idProduct);
+// utilisation de la méthode URLSearchParams pour récuperer l'iD du produit
+const getProductId = () => {
+  //nous retourne l'URL
+  return new URL(location.href).searchParams.get("id");
+};
+const productId = getProductId();
 
 //récupération des produits dans l'API grâce à la méthode fetch
-fetch("http://localhost:3000/api/products/")
+fetch(`http://localhost:3000/api/products/${productId}`)
   //Si Api fonction resp.ok en .json
   .then((response) => response.json())
   .then((dataBase) => {
@@ -23,39 +19,38 @@ fetch("http://localhost:3000/api/products/")
   .catch((error) => {
     alert(error);
   });
-//selection de l'iD Quantity
-const selectQuantity = document.querySelector("colors");
-
-//selection del'iD Colors
-const selectColors = document.querySelector("#quantity");
-// ajout du bouton Ajouter au panier
-const button = document.querySelector("#addToCart");
 
 /*-------------------------------------------------------------
         AFFICHAGE PRODUITS
 ------------------------------------------------------*/
 
+//selection de l'iD Colors
+const selectColors = document.querySelector("#colors");
+//selection del'iD Quantity
+const selectQuantity = document.querySelector("#quantity");
+// ajout du bouton Ajouter au panier
+const button = document.querySelector("#addToCart");
+
 //fonction qui récupère les données de ma promesse dataBase
 let selectProduct = (dataBase) => {
   //ajout des données HTML
-  document.querySelector("head>title").textContent = product.title;
+  document.querySelector("head > title").textContent = dataBase.title;
   document.querySelector(
     ".item__img"
   ).innerHTML += `<img src="${dataBase.imageUrl}" alt="${dataBase.altTxt}">`;
   //ajout prix, titre et description
   document.querySelector("#price").textContent += dataBase.price;
   document.querySelector("#title").textContent += dataBase.title;
-  document;
-  querySelector("#descrpition").textContent += dataBase.description;
+  document.querySelector("#description").textContent += dataBase.description;
 
   //Boucle for of pour chercher les différentes couleurs ( en fonction de sa valeur et de sa clef) dans le HTML
-  for (let selectColors of dataBase.colors) {
+  for (let color of dataBase.colors) {
     //création d'un élément "option"
     let option = document.createElement("option");
     // option de couleurs en fonction des valeurs
-    option.innerHTML = "${productColors}";
-    option.value = "${productColors}";
-    selectColors.apprendChild(option);
+    option.innerHTML = `${color}`;
+    option.value = `${color}`;
+    selectColors.appendChild(option);
 
     console.log("afficher les couleurs disponible du kanap");
     console.log(option);
@@ -64,10 +59,14 @@ let selectProduct = (dataBase) => {
 /* ------------------------------------------------------------------
                   AJOUT OBJET ET BOUTON
 --------------------------------------------------------------*/
-const registerProductOnLocalStorage = (dataBase) => {
+let registerProductOnLocalStorage = (dataBase) => {
+  //utilisation de la méthode addEventListener pour ajouter le clik du bouton
   button.addEventListener("clik", (event) => {
     event.preventDefault();
+
     if (selectColors.value == false) {
+      confirm("Veuillez selectionner une couleur");
+    } else if (selectQuantity.value == 0) {
       confirm("Veuillez sélectionner le nombre d'articles souhaités");
     } else {
       alert("Votre article a bien été ajouté au panier");
@@ -75,7 +74,7 @@ const registerProductOnLocalStorage = (dataBase) => {
                   AJOUT PRODUIT DANS LE PANIER
 --------------------------------------------------------------*/
       //on récupère les valeurs des produits du panier
-      let optionProduct = {
+      let selectProduct = {
         name: dataBase.title,
         id: dataBase._id,
         img: dataBase.imageUrl,
@@ -84,12 +83,12 @@ const registerProductOnLocalStorage = (dataBase) => {
         color: dataBase.value,
         qantity: parseInt(selectQuantity.value, 10),
       };
-      console.log("vérification des options du panier");
-      console.log(selectQuantity);
+      console.log(selectProduct);
 
       /* ------------------------------------------------------------------
                   AJOUT DU LOCAL STORAGE
 --------------------------------------------------------------*/
+
       // récupération des valeurs des produits dans le local storage , utilisation de JSON.parse pour convertir les données json du LS en éléments JS
       let basket = JSON.parse(localStorage.getItem("cart"));
       // si il y à un produit dans le LS
@@ -98,7 +97,7 @@ const registerProductOnLocalStorage = (dataBase) => {
         //utilisation de la méthode find pour analyser les options des articles
         let articles = basket.find(
           (articles) =>
-            articles._id == selectProduct._id &&
+            articles.id == selectProduct.id &&
             articles.color == selectProduct.color
         );
         //si oui, ajout nouvelle quantité et , mise à jour du totalprice
@@ -108,7 +107,6 @@ const registerProductOnLocalStorage = (dataBase) => {
           localStorage.setItem("cart", JSON.stringify(basket));
           console.log("Quantité ajouté au panier");
           return;
-          ``;
         }
         //si pas d'ajout de quantités , alors on push le prochain article dans le panier
         basket.push(selectProduct);
